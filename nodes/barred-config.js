58f5a448deff6dac6727f1ccb673c7310efbc9cf
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2025-10-15 07:03:44 +0100

    Update barred-config.js

--- nodes/barred-config.js
+++ nodes/barred-config.js
@@ -1,8 +1,9 @@
+const { Server } = require("socket.io");
 module.exports = function (RED) {
 	function Config(Config) {
 		RED.nodes.createNode(this, Config);
 		const self = this;
-
+		self.io = new Server(server);
 		
 
 		self.on('close', (removed, done) => {
